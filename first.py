6653398ed97e75b35820698cfe604b74d4fbcefd
qifumin <fumin.qi@aisuperstar.com>
2020-06-15 20:00:59 +0800

    update

--- first.py
+++ first.py
@@ -1,3 +1,19 @@
-import time
+import asyncio
+# import
+async def ws_live_jd(room_id):
+    uri = 'wss://xk.newrank.cn/xdnphb/nr/cloud/ks/websocket'
+    async with websockets.connect(uri, ssl=True) as websocket:
+        content = {"type": "webcast", "data": {"room_id": room_id}}
 
-print(time.time())
+        await websocket.send(json.dumps(content))
+        while True:
+            res = await websocket.recv()
+            message = json.loads(res)
+            print(message)
+
+import requests
+
+if __name__ == '__main__':
+    # room_id = "iishYjkug-s"
+    room_id = '9eF6XnbLa3M'
+    asyncio.get_event_loop().run_until_complete(ws_live_jd(room_id))
